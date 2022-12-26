f17b4ea5fe81cf0de30c59bd4927dd274cf50389
Smirnov Mihail <118544468+avt323290-2@users.noreply.github.com>
2022-12-26 13:09:37 +0300

    Добавили решение второй задачи

--- dz.9.2/Program.cs
+++ dz.9.2/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Write("Задайте значение числа M: ");
+int m = Convert.ToInt32(Console.ReadLine()); //Конвертируем в число
+
+Console.Write("Задайте значение числа N: ");
+int n = Convert.ToInt32(Console.ReadLine()); //Конвертируем в число
+
+SumMToN(m, n);
+
+void SumMToN(int m, int n) // Функциия "сумма чисел от M до N"
+{
+    Console.Write("Сумма чисел от M до N равна: " + SumMN(m - 1, n));
+}
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
